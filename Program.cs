38c6e9b42b07ce24ecd409c81c7309989b3d7ae5
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-06-18 11:31:08 -0300

    começa :P

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class DescontoAtacadao
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(@"Bem vindo ao Atacadão Moura!
+        Pressione uma tecla para ver o catálogo...");
+
+        //Console.WriteLine(@"GTIN          Descrição          Preço (varejo)     Preço (atacado)     Unidades (atacado)
+        //7891024110348
+        //7891048038017
+        //7896066334509
+        //7891700203142
+        //7894321711263	
+        //7896001250611
+        //7793306013029
+        //7896004400914
+        //7898080640017	
+        //7891025301516
+        //7891030003115	");
+    }
+}
